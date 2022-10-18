4070a0cd2a684c5273516715ed5fdc233abfd330
Dankó Attila <dankoattila92@gmail.com>
2022-10-18 22:21:07 +0200

    comment

--- hex.js
+++ hex.js
@@ -2,6 +2,8 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
+//button clicking event -> 
+//function generates a random hex color using for loop hard coded '#' character 
 btn.addEventListener("click", function () {
     let hexColor = "#";
     for (let i = 0; i < 6; i++) {
