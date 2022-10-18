28f8ab5d975d25b36666562a375af7a5a5e84fc5
Dankó Attila <dankoattila92@gmail.com>
2022-10-18 22:12:54 +0200

    add js logic color flipper

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["#E9EDC9", "#FEFAE0", "#FCF4D7"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+const maxArray = colors.length;
+
+btn.addEventListener("click", function () {
+    //random number from colors array (0-3)
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+})
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * maxArray);
+}
